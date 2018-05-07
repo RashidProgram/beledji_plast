48bab2aab71318487da9f69dcca24578a319a2b2
user4 <user4@mail.ru>
2018-05-07 19:13:30 +0300

    bel_plast

---
+++ www/index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link href="styles/menu.css" rel="stylesheet">
+    <link href="styles/style.css" rel="stylesheet">
+    <script src="scripts/jquery.js"></script>
+    <script src="scripts/site.js"></script>
+    <title>Беледжи пласт</title>
+</head>
+<body>
+<header>
+    <div id="headerContent">
+        <div id="logo"></div>
+        <div id="companyName">Беледжи пласт</div>
+        <div id="horisontalMenu">
+            <a href="#" class="active">
+                Главная
+            </a>
+            <a href="shop.html">
+                Магазин
+            </a>
+        </div>
+    </div>
+</header>
+
+<div id="content">
+    <?php
+  $variable
+    ?>
+    <div id="promo">
+        <h1 id="promoText">
+            Здесь будет рекламный заголовок
+        </h1>
+    </div>
+
+    <div id="mainTextWrap">
+        <div id="mainText">
+            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
+        </div>
+    </div>
+
+     <div style="width: 1100px; margin: 0 auto">
+        <hr>
+        <h2>
+            Это будет отдельная страница с открытым магзином
+        </h2>
+
+        <div style="width: 1100px; margin: 0 auto">
+            <h1>
+                Каталог товаров
+            </h1>
+            <div id ='shop'>
+                <div class="shopUnit">
+                    <img src="http://placehold.it/308x231" />
+
+                    <div class="shopUnitName">
+                        Название продукта
+                    </div>
+                    <div class="shopUnitShortDesc">
+                        Здесь будет немного текста описывающего продукт.
+                        Здесь будет немного текста описывающего продукт.
+                        Здесь будет немного текста описывающего продукт.
+                    </div>
+                    <div class="shopUnitPrice">
+                        Цена: 0000 $
+                    </div>
+                    <a href="#" class="shopUnitMore">
+                        Подробнее
+                    </a>
+                </div>
+
+              <div class="shopUnit">
+                    <img src="http://placehold.it/308x231" />
+
+                    <div class="shopUnitName">
+                        Название продукта
+                    </div>
+                    <div class="shopUnitShortDesc">
+                        Здесь будет немного текста описывающего продукт.
+                        Здесь будет немного текста описывающего продукт.
+                        Здесь будет немного текста описывающего продукт.
+                    </div>
+                  <div class="shopUnitPrice">
+                        Цена: 0000 $
+                    </div>
+                    <a href="#" class="shopUnitMore">
+
+                   Подробнее
+                    </a>
+                </div>
+
+                <div class="shopUnit">
+                    <img src="http://placehold.it/308x231" />
+                    <div class="shopUnitName">
+                    Название продукта
+                </div>
+                <div class="shopUnitShortDesc">
+                    Здесь будет немного текста описывающего продукт.
+                      Здесь будет немного текста описывающего продукт.
+                      Здесь будет немного текста описывающего продукт.
+                </div>
+                <div class="shopUnitPrice">
+                        Цена: 0000 $
+                    </div>
+                    <a href="#" class="shopUnitMore">
+                        Подробнее
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div style="margin-bottom: 20px; float: left;">
+            <hr>
+            <h2>
+                Это будет отдельная страница с открытым товаром
+            </h2>
+
+            <div id="openedProduct-img">
+                <img src="http://placehold.it/342x260">
+            </div>
+            <div id="openedProduct-content">
+                <h1 id="openedProduct-name">
+                    Название продукта
+                </h1>
+                <div id="openedProduct-desc">
+                    Здесь будет описание продукта. Здесь будет описание продукта. Здесь будет описание продукта.
+                    Здесь будет описание продукта. Здесь будет описание продукта. Здесь будет описание продукта.
+                    Здесь будет описание продукта. Здесь будет описание продукта. Здесь будет описание продукта.
+                </div>
+                <div id="openedProduct-price">
+                    Цена: 0000 $
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</div>
+
+<footer>
+    <div id="footerInside">
+
+        <div id="contacts">
+            <div class="contactWrap">
+                <img src="images/envelope.svg" class="contactIcon">
+                info@brandshop.ru
+            </div>
+            <div class="contactWrap">
+                <img src="images/phone-call.svg" class="contactIcon">
+                8 800 555 00 00
+            </div>
+            <div class="contactWrap">
+                <img src="images/placeholder.svg" class="contactIcon">
+                Москва, пр-т Ленина, д. 1 офис 304
+            </div>
+        </div>
+
+        <div id="footerNav">
+            <a href="#">Главная</a>
+            <a href="#">Магазин</a>
+        </div>
+
+        <div id="social">
+            <img class="socialItem" src="images/vk-social-logotype.svg">
+            <img class="socialItem" src="images/google-plus-social-logotype.svg">
+            <img class="socialItem" src="images/facebook-logo.svg">
+        </div>
+
+        <div id="copyrights">&copy; Brandshop</div>
+    </div>
+</footer>
+    <button onclick = 'getImg(prompt("Админ введи id картины"),prompt("Админ введи название картины"),prompt("Админ введи формат картины"))'>Добавить фото</button>
+<script>
+  function getImg(idImg,nameImg,formatImg){
+      idImg.toLowerCase();
+      nameImg.toLowerCase();
+      formatImg.toLowerCase();
+       var NewShopUnit = document.createElement('div') ;
+       var NewShopUnitName = document.createElement('div') ;
+       var NewShopUnitShortDesc = document.createElement('div') ;
+       var NewShopUnitPrice = document.createElement('div') ;
+       var NewShopUnitMore = document.createElement('a') ;
+                    NewShopUnitName.innerHTML = 'Название товара';
+                    NewShopUnitPrice.innerHTML = 'Цена: 0000 Р';
+                    NewShopUnitMore.innerHTML = ' Подробнее';
+                    NewShopUnitShortDesc.innerHTML = 'Здесь будет немного текста описывающего продукт.Здесь будет немного текста описывающего продукт.Здесь будет немного текста описывающего продукт.';
+          $(NewShopUnit).attr('class','shopUnit') ;
+          $(NewShopUnitName).attr('class','shopUnitName') ;
+          $(NewShopUnitShortDesc).attr('class','shopUnitShortDesc') ;
+          $(NewShopUnitPrice).attr('class','shopUnitPrice') ;
+          $(NewShopUnitMore).attr('class','shopUnitMore') ;
+          $(NewShopUnitMore).attr('href','#') ;
+           {     var shop = document.getElementById('shop');
+                var imageShopUnit = document.createElement('img') ;
+                $(imageShopUnit).attr('src','images/'+nameImg+'.'+formatImg) ; 
+                NewShopUnit.id = idImg; }
+      
+                NewShopUnit.insertBefore(NewShopUnitMore,NewShopUnit.firstChild);
+                NewShopUnit.insertBefore(NewShopUnitPrice,NewShopUnit.firstChild);
+                NewShopUnit.insertBefore(NewShopUnitShortDesc,NewShopUnit.firstChild);
+                NewShopUnit.insertBefore(NewShopUnitName,NewShopUnit.firstChild);
+                NewShopUnit.insertBefore(imageShopUnit,NewShopUnit.firstChild);
+                shop.insertBefore(NewShopUnit, shop.firstChild );
+                 /* <div class="shopUnitPrice">
+                        Цена: 0000 $
+                    </div>
+                    <a href="#" class="shopUnitMore">
+
+                   Подробнее
+                    </a>*/
+  }  
+
+</script>
+    
+</body>
+</html>
